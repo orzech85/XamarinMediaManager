ddcb8b7584ce4509d5d951347f738b40104667e4
Michal Orzechowski <michal@orzechowski.info.pl>
2020-04-30 09:29:43 +0200

    Override view presenter

--- Samples/ElementPlayer.Android/SplashScreen.cs
+++ Samples/ElementPlayer.Android/SplashScreen.cs
@@ -1,6 +1,10 @@
-﻿using Android.App;
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Android.App;
 using Android.Content.PM;
+using AndroidX.AppCompat.App;
 using MvvmCross.DroidX.AppCompat;
+using MvvmCross.Platforms.Android.Presenters;
 
 namespace ElementPlayer.Android
 {
@@ -12,11 +16,36 @@ namespace ElementPlayer.Android
         Theme = "@style/AppTheme.Splash",
         LaunchMode = LaunchMode.SingleTop,
         ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
-    public class SplashScreen : MvxSplashScreenAppCompatActivity<MvxAppCompatSetup<Core.App>, Core.App>
+    public class SplashScreen : MvxSplashScreenAppCompatActivity<Setup, Core.App>
     {
         public SplashScreen()
              : base(Resource.Layout.splash_screen)
         {
+        }
+    }
+
+    public class Setup : MvxAppCompatSetup<Core.App>
+    {
+        protected override IMvxAndroidViewPresenter CreateViewPresenter()
+        {
+            var presenter = new MyAndroidViewPresenter(AndroidViewAssemblies);
+            return presenter;
+        }
+    }
+
+    public class MyAndroidViewPresenter : MvxAndroidViewPresenter
+    {
+        public MyAndroidViewPresenter(IEnumerable<Assembly> androidViewAssemblies) : base(androidViewAssemblies)
+        {
+        }
+
+        protected override AppCompatActivity CurrentActivity
+        {
+            get
+            {
+                var activity = base.CurrentActivity;
+                return activity;
+            }
         }
     }
 }
